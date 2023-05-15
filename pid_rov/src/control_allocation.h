a2e3e604c3469b7818a8346edff767ebd885f1b9
SimoneCarena <simonecarena2000@gmail.com>
2023-05-15 11:44:34 +0200

    added code for the pid (including a main to test it)

---
+++ pid_rov/src/control_allocation.h
@@ -0,0 +1,10 @@
+#ifndef _CONTROL_ALLOCATION_H
+#define _CONTROL_ALLOCATION_H
+
+#define Z_COEFFICIENT 0.2500
+#define UPPER_LIMIT 44.3639 
+#define LOWER_LIMIT -34.4651
+
+double map_to_kgf(double force, double tol);
+
+#endif
