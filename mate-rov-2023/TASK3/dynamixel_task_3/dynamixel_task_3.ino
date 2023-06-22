ca2baacbb3f8ac8ae73c1edd745f9780b1c4241d
MauroFoti <42967448+MauroFoti@users.noreply.github.com>
2023-06-21 19:22:49 -0600

    Updated float firmware

--- mate-rov-2023/TASK3/dynamixel_task_3/dynamixel_task_3.ino
+++ mate-rov-2023/TASK3/dynamixel_task_3/dynamixel_task_3.ino
@@ -1,45 +1,92 @@
-  
+
+/*TASK 3 DESCRIPTION
+ * 
+ * 1 - COMMUNICATION WITH THE WORKING STATION PRIOR DESCENDING (10 PTS)
+ * 
+ * 2 - FIRST DESCENDING -> WE SEND A COMMAND SIGNAL TO THE ESP THAT STARTS THE DESCENDING (10 PTS)
+ * 
+ * 3 - COME BACK TO THE SURFACE AND SEND DATA AGAIN (10 PTS)
+ * 
+ * 4 - SECOND DESCENDING -> WE SEND ANOTHER SIGNAL TO THE ESP THAT START THE DESCENDING (15 PTS)
+ * 
+ * 5 - COME BACK TO THE SURFACE AND SEND DATA AGAIN (10 PTS)
+ * 
+ */
 #include <DynamixelPolitocean.h>
 #include <string.h>
 #include "RTClib.h"
 
-
+void printTime();
+void goUp();
+void goDown();
+String cmd;
 RTC_PCF8523 rtc; 
 DynamixelPolitocean dxl(Wire,0x4D, 1); 
+DateTime now = rtc.now();
+
+#define DESCENDING_TIME 10000 //ms to complete a full descend -> GO DOWN
+#define ASCENDING_TIME 10000  //ms to complete a full ascend  -> GO UP
 void setup() { 
   
-  Serial.begin(9600); 
+  Serial.begin(57600); 
   //Serial.println("here");
  
   delay(100); 
   dxl.begin(57600); 
   delay(100); 
   dxl.setOperatingMode(WHEEL); 
-  //dxl.TorqueOn(); 
-  //dxl.setVelocity(1023);
 
    while(!rtc.begin()) { 
-    Serial.println("verifica connessioni"); 
-    //while(true);
+    Serial.println("RTC NOT WORKING"); 
     delay(200);
   } 
  
   if (rtc.lostPower()) { 
+    //enter if the status registers says that a power loss has occurred, NECESSARY
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); 
   } 
   
 } 
  
 void loop() { 
-//Serial.println("here_loop");
-// delay(4000); 
-//  dxl.setVelocityWheelMode(1023, CW);
-//  delay(2000);
-//  dxl.setVelocityWheelMode(1000,CCW);
-//  delay(2000);
-   
-  DateTime now = rtc.now(); 
-  dxl.LedOn();
+
+/*
+ *  LOOP DISPLAYING TIME AND ID, ONCE EVERY SECOND
+ *  UNTIL THE COMMAND "GO" HAS BEEN SENT TO THE ESP -> THIS STOP THE TRANSMISSION AND THEN PROCEEDS WITH THE DESCENDING, THEN CONTINUE TO SEND DATA
+ */
+
+ //FIRST PRINT OUR ID
+ Serial.print("EX04");
+
+ //NOW PRINT THE TIME
+ printTime();   //print the data to the esp monitorà
+ delay(1000);
+
+
+ //CHECKING IF DESCENDING COMMAND IS READY
+ if(Serial.available() != 0)
+ {
+    cmd = Serial.readString();  //RECEIVED COMMAND
+
+    if(cmd == "GO")   //DECODE SIGNAL
+    {
+      //DESCENDING
+      goDown();
+      delay(DESCENDING_TIME);
+
+      //ASCENDING
+      goUp();
+      delay(ASCENDING_TIME);      
+    }
+ }
+  
+}
+
+void printTime()
+{
+  //this function only prints the time (AS SPECIFIED BY THE MATE ROV - TASK 3)
+  DateTime now = rtc.now();
+  
   Serial.print(now.year(), DEC); 
   Serial.print("/"); 
   Serial.print(now.month(), DEC); 
@@ -51,42 +98,17 @@ void loop() {
   Serial.print(now.minute(), DEC); 
   Serial.print(':'); 
   Serial.print(now.second(), DEC); 
-  Serial.println();  
-  delay(2000);
-  //dxl.setVelocityWheelMode(1023, CCW);
-  now = rtc.now(); 
-  dxl.LedOff();
-  Serial.print(now.year(), DEC); 
-  Serial.print("/"); 
-  Serial.print(now.month(), DEC); 
-  Serial.print("/"); 
-  Serial.print(now.day(), DEC); 
-  Serial.print(" "); 
-  Serial.print(now.hour(), DEC); 
-  Serial.print(':'); 
-  Serial.print(now.minute(), DEC); 
-  Serial.print(':'); 
-  Serial.print(now.second(), DEC); 
-  Serial.println(); 
-  delay(2000);
+  Serial.println();
+}
 
-//  DateTime now = rtc.now(); 
-// 
-//  Serial.print(now.year(), DEC); 
-//  Serial.print("/"); 
-//  Serial.print(now.month(), DEC); 
-//  Serial.print("/"); 
-//  Serial.print(now.day(), DEC); 
-//  Serial.print(" "); 
-//  Serial.print(now.hour(), DEC); 
-//  Serial.print(':'); 
-//  Serial.print(now.minute(), DEC); 
-//  Serial.print(':'); 
-//  Serial.print(now.second(), DEC); 
-//  Serial.println(); 
- 
+void goUp()
+{
+  //GOING UP -> ROTATE CCW
+  dxl.setVelocityWheelMode(1023, CCW);
+}
 
-  
-  
- 
+void goDown()
+{
+  //GOING DOWN -> ROTATE CW
+  dxl.setVelocityWheelMode(1023, CW);
 }
